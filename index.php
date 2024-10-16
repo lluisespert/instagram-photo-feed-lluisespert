5be3a6debbd7e967c617cb5ab3ed22579ae4f9f8
Lluis Espert Cuquerella <lluisespertc@gmail.com>
2024-10-16 19:13:04 +0200

    Primera versión terminada

---
+++ index.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html>
+
+<html>
+
+    <head>
+
+        <title>Instagram</title>
+
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <link rel="stylesheet" type="text/css" href="./css/estilos.css" />
+
+    </head>
+
+    <body class="fondo">
+
+        <h1 class="centrarTexto">Breathe Code Photo Feed</h1>
+
+
+        <div class="box">
+
+            <div>
+                
+            <p>Cabra 1</p>
+
+            <img src="./img/cabra1.jpg"  class="tamanyoImagen">
+
+            <p>Autor: Lluís Espert Cuquerella</p>
+            
+            </div>
+
+        </div>
+
+        <div class="box">
+
+            <div>
+    
+                <p>Cabra 2</p>
+
+                <img src="./img/cabra2.jpg"  class="tamanyoImagen">
+
+                <p>Autor: Lluís Espert Cuquerella</p>
+
+            </div>
+
+        </div>
+
+        <div class="box">
+
+            <div>
+    
+                <p>Cabra 3</p>
+
+                <img src="./img/cabra3.jpg"  class="tamanyoImagen">
+
+                <p>Autor: Lluís Espert Cuquerella</p>
+
+            </div>
+
+        </div>
+
+        <div class="box">
+
+            <div>
+    
+                <p>Cabra 4</p>
+
+                <img src="./img/cabra4.jpg"  class="tamanyoImagen">
+
+                <p>Autor: Lluís Espert Cuquerella</p>
+
+            </div>
+
+        </div>
+
+        <div class="box">
+
+            <div>
+    
+                <p>Cabra 5</p>
+
+                <img src="./img/cabra5.jpg"  class="tamanyoImagen">
+
+                <p>Autor: Lluís Espert Cuquerella</p>
+
+            </div>
+
+        </div>
+
+        <div class="box">
+
+            <div>
+    
+                <p>Cabra 6</p>
+
+                <img src="./img/cabra6.jpg"  class="tamanyoImagen">
+
+                <p>Autor: Lluís Espert Cuquerella</p>
+
+            </div>
+
+        </div>
+
+    <div class="box">
+
+        <div>
+    
+            <p>Cabra 7</p>
+
+            <img src="./img/cabra7.jpg"  class="tamanyoImagen">
+
+            <p>Autor: Lluís Espert Cuquerella</p>
+
+        </div>
+
+    </div>
+
+    <div class="box">
+
+        <div>
+    
+            <p>Cabra 8</p>
+
+            <img src="./img/cabra8.jpg"  class="tamanyoImagen">
+
+            <p>Autor: Lluís Espert Cuquerella</p>
+
+        </div>
+
+    </div>
+
+    </body>
+
+</html>
